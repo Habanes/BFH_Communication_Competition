53f3d6f15f970e8073d0710c89aa370ce2451846
Felix Egger <eggef6@bfh.ch>
2025-11-20 10:54:26 +0100

    added channel file

---
+++ transmission_competition/Channel.py
@@ -0,0 +1,13 @@
+"""
+File: Channel.py
+Author: Felix Egger
+Description: Implements the Channel class for transmission.
+"""
+
+import numpy as np
+
+class Channel:
+    def __init__(self):
+        pass
+    
+    
--- transmission_competition/QPSK.py
+++ transmission_competition/QPSK.py
@@ -17,3 +17,19 @@ class QPSK:
         }
         
         self.inverse_mapping = {v: k for k, v in self.mapping.items()}
+    
+    def Modulation(self, bit_pairs: np.ndarray) -> np.ndarray:
+        
+        Symbols = np.array([self.mapping[tuple(pair)] for pair in bit_pairs])
+        
+        return np.array(Symbols)
+    
+    def Demodulation(self, received_symbols: np.ndarray) -> np.ndarray:
+        
+        demodulated_bits = []
+        for r in received_symbols:
+            distances = {symbol: np.abs(r - mapped_symbol) for symbol, mapped_symbol in self.mapping.items()}
+            closest_symbol = min(distances, key=distances.get)
+            demodulated_bits.append(closest_symbol)
+        
+        return np.array(demodulated_bits)
